0cf58e5c98246fb951de6082ca85ea0f97f6116d
VictorEmanuel90 <victor.fundamentos@gmail.com>
2022-05-27 18:39:32 -0300

    template admin

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 // use \Hcode\Page;
 use Hcode\Pages;
+use Hcode\PagesAdmin;
 
 $app = new Slim();
 
@@ -13,6 +14,13 @@ $app->config('debug', true);
 $app->get('/', function () {
 
 	$page = new Pages();
+
+	$page->setTpl("index");
+});
+
+$app->get('/admin', function () {
+
+	$page = new PagesAdmin();
 
 	$page->setTpl("index");
 });
---
+++ vendor/hcodebr/php-classes/src/pagesAdmin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hcode;
+
+class PagesAdmin extends Pages {
+
+    public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+    {
+        parent::__construct($opts, $tpl_dir);
+    }
+}
+
+?>
